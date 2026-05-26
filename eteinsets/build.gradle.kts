0e6c45dda9a407ed1f5dcbfbe7df7554fa087c0b
dapadz <dapadz@vk.com>
2026-05-26 13:25:02 +0300

    up version

--- eteinsets/build.gradle.kts
+++ eteinsets/build.gradle.kts
@@ -65,7 +65,7 @@ afterEvaluate {
             create<MavenPublication>("maven") {
                 groupId = "ru.dapadz"
                 artifactId = "eteinsets"
-                version = "1.0.2"
+                version = "1.1.0"
 
                 from(components["release"])
 
